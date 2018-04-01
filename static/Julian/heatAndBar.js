b33747d2f769077b0b66b84f462ee91689f415c8
Julian Johnson <julianjohnson88@gmail.com>
2018-04-01 15:59:55 -0700

    added additional data set

--- static/Julian/heatAndBar.js
+++ static/Julian/heatAndBar.js
@@ -1,5 +1,6 @@
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-years = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+years = ["2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"]
+years2 = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
 
 var darkMapBox = L.tileLayer("https://api.mapbox.com/styles/v1/julianalexander/cjez3ryi74tjx2rqrvnbsiykr/tiles/256/{z}/{x}/{y}?"+
 "access_token=pk.eyJ1IjoianVsaWFuYWxleGFuZGVyIiwiYSI6ImNqZXZwM2puNTBwO"+
@@ -27,7 +28,7 @@ var xBandScale = d3
   .padding(0.1);
 
 // Define a linearScale which will be used to create our left axis and position and size our bars along the y-axis
-var yLinearScale = d3.scaleLinear().domain([0,650]).range([chartHeight, 0]);
+var yLinearScale = d3.scaleLinear().domain([0,1600]).range([chartHeight, 0]);
 // Configure a bottom axis function using the d3.axisBottom method and the band scale.
 var bottomAxis = d3.axisBottom(xBandScale);
 // Configure a left axis function using the d3.axisBottom method and the band scale.
@@ -35,17 +36,45 @@ var leftAxis = d3.axisLeft(yLinearScale);
 
 var colors = ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"];
 
-
+var n = 1;
 
 data07 = [];
 death07 = [];
 death_07 = [];
+countyDeaths = [];
 heatArray = [];
 timeArray = [];
+gunData = [];
 
 d3.json("http://127.0.0.1:5000/gunData", function(error, data){
+    if (error) console.warn(error);
+        console.log(data);
+        gunData = data;
+        countyDeaths.push(data.filter(function(obj){
+            return obj.Year === years2[0]
+        }))
+        for (var i = 0; i < countyDeaths[0].length; i++){
+            timeArray.push([countyDeaths[0][i].latitude, countyDeaths[0][i].longitude, countyDeaths[0][i].Deaths])      
+        };
+        console.log(timeArray)
+        
+         var heat = L.heatLayer(timeArray,{
+            radius: 25,
+            blur: 20,
+            minOpacity:0.65,
+            maxZoom: 18,
+            gradient:{
+                '0.0':'green',
+                '0.5': 'blue',
+                '1.0':'red'
+            }
+        }).addTo(myMap);  
+        }) 
+
+
+ d3.json("http://127.0.0.1:5000/totalGunDeaths", function(error, data){
     if (error) console.warn(error); //loading the data
-    console.log(data);
+    console.log(data); 
 
     data07.push(data.filter(function(obj){
         return obj.Year === years[0];
@@ -53,40 +82,8 @@ d3.json("http://127.0.0.1:5000/gunData", function(error, data){
     console.log(data07)
 
     for (var i = 0; i < data07[0].length; i++){
-        death07.push([data07[0][i].Deaths, data07[0][i].Month]);
-        timeArray.push([data07[0][i].latitude, data07[0][i].longitude, data07[0][i].Deaths])      
+        death07.push([data07[0][i].Deaths, data07[0][i].Month]);     
     };
-    ///configuring heatmap
-/*     var svgMap = d3
-    .select("#svg-map")
-    .append("svg")
-    .attr("height", svgHeight)
-    .attr("width", svgWidth) */
-
-     var heat = L.heatLayer(timeArray,{
-        radius: 25,
-        blur: 20,
-        minOpacity:0.65,
-        maxZoom: 18,
-        gradient:{
-            '0.0':'green',
-            '0.5': 'blue',
-            '1.0':'red'
-        }
-       }).addTo(myMap);  
-    
-    //for (var i = 0; i < data.length; i++){
-    //   var location = data[i].latitude;
-    //   var month = data[i].Month;
-    //   var year = data[i].Year
-    //   console.log(year)
-    //   if (location) {
-          // heatArray.push([data[i].latitude, data[i].longitude, data[i].Deaths])
-          // timeArray.push([data[i].Month, data[i].Year])
-    //   }
-    //}
-
-      ////configuring bar chart
 
     var deathsTotal = d3.nest()
         .key(function(d) { return d.Month; })
@@ -156,17 +153,6 @@ svg.append("text")
 d3.select("#total")
 .data(death_07)
 .text(death_07.reduce((a,b) => a + b, 0))
-  //adding tooltip
-
-/*   var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-50, 0])
-  .html(function(d) {
-     return "<strong>Month:</strong> <span style='color:red'>" + data07[0][0].Month + "</span>" + "</br>" + 
-     "<strong>Year:</strong> <span style='color:red'>" + data07[0][0].Year + "</span>" + "</br>"
-   })
-
-svg.call(tip); */
 
 svg
   .selectAll(".bar")
@@ -190,8 +176,7 @@ svg
   .style("fill",function(d){
       return colorScale(d)
   })
- // .on("mouseover", tip.show)
- // .on("mouseout", tip.hide);
+
 
   svg
   .selectAll(".text")
@@ -199,7 +184,7 @@ svg
   .enter()
   .append("text")
   .attr("class", "text")
-  .attr("text-anchor","right")
+  .attr("text-anchor","middle")
   .attr("x", function(data, index) {
     return xBandScale(months[index]);
   })
@@ -213,27 +198,35 @@ svg
   .attr("dx", "2.5em")
   ;
 
-
-var j = 1;
-
-
   d3.select("#nextYear")
     .on("click", function(){
 
         dataYear = [];
         death_year = [];
         deathYear = [];
-
+        gunDataYear = [];
         newTimeArray = [];
+        
 
+        console.log(gunData)
+        console.log(data)
+    
         dataYear.push(data.filter(function(obj){
-            return obj.Year === years[j];
+            return obj.Year === years[n];
         }))
         console.log(dataYear)
+
+        gunDataYear.push(gunData.filter(function(obj){
+            return obj.Year === years2[n];
+        }))
+        console.log(gunDataYear)
+
         for (var i = 0; i < dataYear[0].length; i++){
-            death_year.push([dataYear[0][i].Deaths, dataYear[0][i].Month])
-            newTimeArray.push([dataYear[0][i].latitude, dataYear[0][i].longitude, dataYear[0][i].Deaths])      
+            death_year.push([dataYear[0][i].Deaths, dataYear[0][i].Month])      
         };
+        for (var j = 0; j < gunDataYear[0].length; j++){
+            newTimeArray.push([gunDataYear[0][j].latitude, gunDataYear[0][j].longitude, gunDataYear[0][j].Deaths])
+        }
         console.log(newTimeArray)
         var deathsTotal = d3.nest()
             .key(function(d) { return d.Month; })
@@ -248,8 +241,8 @@ var j = 1;
         console.log(death_year);
     
         console.log(deathYear);
-        j++;
-        console.log(j)
+        n++;
+        console.log(n)
 
         //update heatmap
         var heat = L.heatLayer(newTimeArray,{
@@ -283,17 +276,17 @@ var j = 1;
         .style("fill",function(data){
             return colorScale(data)
         });
-         console.log(years[j-1]);
+         console.log(years2[n-1]);
 
         d3
         .select("#year")
-        .data(years)
-        .text(years[j-1] + "*")
+        .data(years2)
+        .text(years2[n-1] + "*")
 
         d3
         .select("#year2")
-        .data(years)
-        .text(years[j-1]+":")
+        .data(years2)
+        .text(years2[n-1]+":")
 
         svg
         .selectAll(".text")
