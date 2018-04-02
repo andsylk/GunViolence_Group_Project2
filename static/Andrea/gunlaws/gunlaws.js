fd7ed40bdded5a869dd217b3fa1e6ec4f83df6f4
Andrea Kwong <andrea.kwong@gmail.com>
2018-04-02 16:03:54 -0700

    make changes9

--- static/Andrea/gunlaws/gunlaws.js
+++ static/Andrea/gunlaws/gunlaws.js
@@ -1,6 +1,17 @@
 var mapboxAccessToken = 'pk.eyJ1IjoiYW5kcmVha3dvbmciLCJhIjoiY2pmZ2ZscDRkMmFzcTJ3bWlieGtqOGV2NSJ9.l64uYll6s7hU-IWz8FYW7w'
 ;
-var map = L.map('map').setView([37.8, -96], 5);
+
+
+var maxBounds = [
+    [19.8968, 155.5828], //Southwest
+    [60.905296, -73.478260]  //Northeast
+];
+
+var map = L.map('map')
+    .setView([37.8, -96], 5)
+    .setmaxBounds(maxBounds);
+    
+
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
     attribution: 'Map data &#169; Leaflet',
@@ -42,13 +53,13 @@ function onEachFeature(feature, layer) {
     layer.bindTooltip("<strong>State: </strong>" + feature.properties.name + "<br><strong>Number of Laws: </strong>" + feature.properties.laws);      
         };
 
+
 var legend = L.control({ position: 'bottomright' })
 
 legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend')
     var colors = ['#fff9e6','#ffecb3', '#ffd966', '#ffc61a', '#cc9900','#997300', '#664d00','#332600']
     var labels = [0,1,2,3,4,5,6,7]
-    var color_labels = []
 
     div.innerHTML = '<div class = "labels"><div class ="min">'+ labels[0] +'</div> \
     <div class="max">' + labels[labels.length - 1] + '</div></div>'
