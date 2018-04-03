9719a7bce8869f53a0b9b0a532ba42125411bdd2
Andrea Kwong <andrea.kwong@gmail.com>
2018-04-02 17:58:19 -0700

    change gunlass.js

--- static/Andrea/gunlaws/gunlaws.js
+++ static/Andrea/gunlaws/gunlaws.js
@@ -56,18 +56,19 @@ var legend = L.control({ position: 'bottomright' })
 legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend')
     var colors = ['#fff9e6','#ffecb3', '#ffd966', '#ffc61a', '#cc9900','#997300', '#664d00','#332600']
-    var labels = [0,1,2,3,4,5,6,7]
+    var labels_num = [0,1,2,3,4,5,6,7]
 
-    div.innerHTML = '<div class = "labels"><div class ="min">'+ labels[0] +'</div> \
-    <div class="max">' + labels[labels.length - 1] + '</div></div>'
+    div.innerHTML = '<div class = "labels"><div class ="min">'+ labels_num[0] +'</div> \
+    <div class="max">' + labels_num[labels_num.length - 1] + '</div></div>'
 
-    labels.forEach(function(index)  {
+    var labels = []
+    labels_num.forEach(function(index)  {
         // if (labels.string === '1234567') {continue; }
             labels.push('<li style="background-color: ' + colors[index] + '"></li>')
     })
         console.log(labels)
         var span = document.createElement("span");
-        div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+       div.innerHTML += '<ul>' + labels.join('') + '</ul>'
     
     return div
 };
